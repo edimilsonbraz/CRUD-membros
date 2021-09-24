9718dd8aa414da936c847eae0a986ed846121852
edimilsonbraz <edimilson.gt8@gmail.com>
2021-09-24 14:38:23 -0300

    feature excluir membro ok

--- src/routes.js
+++ src/routes.js
@@ -9,13 +9,14 @@ const basePath = __dirname + '/views';
 // ROTAS
 routes.get("/", (req, res) => res.render('index'))
 
+// LIST MEMBRO
 routes.get("/membros", (req, res) =>  { 
   Addmembro.findAll().then((membro) => {
     res.render('membros', {membro: membro})})
   })
 
 
-//Dados vindo do Formulario
+//CREATE MEMBRO
 routes.post("/membros", (req, res) => {
   Addmembro.create({
     nome: req.body.nome,
@@ -31,7 +32,16 @@ routes.post("/membros", (req, res) => {
   }).catch((erro) => {
     res.send("Erro: Membro não pôde ser cadastrado!")
   })
-  
+})
+
+routes.get('/del-membro/:id', (req, res) => {
+  Addmembro.destroy({
+    where: { 'id': req.params.id }
+  }).then(() => {
+    res.redirect('/membros')
+  }).catch((erro) => {
+    res.send("Membro não pôde ser apagado.")
+  })
 })
 
 module.exports = routes;
